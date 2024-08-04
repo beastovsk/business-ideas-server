883b3bc6b217c91aed1a3a06ea37b4013c173fad
Artyom Narshinov <narshinovartyom@mail.ru>
2024-08-04 13:57:16 +0400

    d

--- contollers/user.controller.js
+++ contollers/user.controller.js
@@ -1,97 +1,111 @@
 const sql = require("../database");
 const bcrypt = require("bcryptjs");
 const { generateToken, decodeToken } = require("../utils");
-const { v4: uuidv4 } = require("uuid")
+const { v4: uuidv4 } = require("uuid");
 
 const userController = {
-  getUser: async (req, res) => {
-    try {
-      const [_, token] = req.headers.authorization.split(" ");
-      const { email } = decodeToken({ token });
-      const result = await sql`SELECT * FROM "users" WHERE email = ${email}`;
-      if (result.length === 0) {
-        return res.status(200).json({ message: "Пользователь не найден" });
-      }
+	getUser: async (req, res) => {
+		try {
+			const [_, token] = req.headers.authorization.split(" ");
+			const { email } = decodeToken({ token });
+			const result =
+				await sql`SELECT * FROM "users" WHERE email = ${email}`;
+			if (result.length === 0) {
+				return res
+					.status(200)
+					.json({ message: "Пользователь не найден" });
+			}
 
-      const { password, is_confirmed, confirm_token, ...user } = result[0];
+			const { password, is_confirmed, confirm_token, ...user } =
+				result[0];
 
-      res.json({ user });
-    } catch (error) {
-      console.log(error);
-      res.status(200).json({ user: null });
-    }
-  },
-  changeEmail: async (req, res) => {
-    try {
-      const { currentEmail, newEmail, password } = req.body;
-      const [_, token] = req.headers.authorization.split(" ");
-      const { email: decodedEmail } = decodeToken({ token });
+			res.json({ user });
+		} catch (error) {
+			console.log(error);
+			res.status(200).json({ user: null });
+		}
+	},
+	changeEmail: async (req, res) => {
+		try {
+			const { currentEmail, newEmail, password } = req.body;
+			const [_, token] = req.headers.authorization.split(" ");
+			const { email: decodedEmail } = decodeToken({ token });
 
-      if (decodedEmail !== currentEmail) {
-        return res.status(200).json({ message: "Некорректная текущая почта" });
-      }
+			if (decodedEmail !== currentEmail) {
+				return res
+					.status(200)
+					.json({ message: "Некорректная текущая почта" });
+			}
 
-      const result =
-        await sql`SELECT * FROM "users" WHERE email = ${decodedEmail}`;
+			const result =
+				await sql`SELECT * FROM "users" WHERE email = ${decodedEmail}`;
 
-      if (result.length === 0) {
-        return res.status(200).json({ message: "Пользователь не найден" });
-      }
+			if (result.length === 0) {
+				return res
+					.status(200)
+					.json({ message: "Пользователь не найден" });
+			}
 
-      const user = result[0];
+			const user = result[0];
 
-      const checkPass = await bcrypt.compare(password, user.password);
-      if (!checkPass) {
-        return res.status(200).json({ message: "Некорректный пароль" });
-      }
+			const checkPass = await bcrypt.compare(password, user.password);
+			if (!checkPass) {
+				return res.status(200).json({ message: "Некорректный пароль" });
+			}
 
-      await sql`UPDATE "users" SET email = ${newEmail} WHERE email = ${currentEmail}`;
+			await sql`UPDATE "users" SET email = ${newEmail} WHERE email = ${currentEmail}`;
 
-      const updatedToken = generateToken({
-        id: user.id,
-        email: newEmail,
-      });
+			const updatedToken = generateToken({
+				id: user.id,
+				email: newEmail,
+			});
 
-      res.json({
-        token: updatedToken,
-        message: "Почта успешно изменена",
-      });
-    } catch (error) {
-      res.status(500).json({ error: "Ошибка сервера" });
-    }
-  },
-  changePassword: async (req, res) => {
-    try {
-      const { currentPassword, password } = req.body;
-      const [_, token] = req.headers.authorization.split(" ");
-      const { email } = decodeToken({ token });
+			res.json({
+				token: updatedToken,
+				message: "Почта успешно изменена",
+			});
+		} catch (error) {
+			res.status(500).json({ error: "Ошибка сервера" });
+		}
+	},
+	changePassword: async (req, res) => {
+		try {
+			const { currentPassword, password } = req.body;
+			const [_, token] = req.headers.authorization.split(" ");
+			const { email } = decodeToken({ token });
 
-      const result = await sql`SELECT * FROM "users" WHERE email = ${email}`;
+			const result =
+				await sql`SELECT * FROM "users" WHERE email = ${email}`;
 
-      if (result.length === 0) {
-        return res.status(200).json({ message: "Пользователь не найден" });
-      }
+			if (result.length === 0) {
+				return res
+					.status(200)
+					.json({ message: "Пользователь не найден" });
+			}
 
-      const user = result[0];
+			const user = result[0];
 
-      const checkPass = await bcrypt.compare(currentPassword, user.password);
-      if (!checkPass) {
-        return res.status(200).json({ message: "Некорректный пароль" });
-      }
+			const checkPass = await bcrypt.compare(
+				currentPassword,
+				user.password
+			);
+			if (!checkPass) {
+				return res.status(200).json({ message: "Некорректный пароль" });
+			}
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+			const hashedPassword = await bcrypt.hash(password, 10);
 
-      await sql`UPDATE "users" SET password = ${hashedPassword} WHERE email = ${email}`;
+			await sql`UPDATE "users" SET password = ${hashedPassword} WHERE email = ${email}`;
 
-      res.json({
-        message: "Пароль успешно изменен",
-      });
-    } catch (error) {
-      res.status(500).json({ error: "Ошибка сервера" });
-    }
-  },
-  createTransaction: async (req, res) => {
-    try {
+			res.json({
+				message: "Пароль успешно изменен",
+			});
+		} catch (error) {
+			res.status(500).json({ error: "Ошибка сервера" });
+		}
+	},
+	createTransaction: async (req, res) => {
+		try {
 			const { paymentMethod, fullName, amount, phoneNumber } = req.body;
 			const [_, token] = req.headers.authorization.split(" ");
 			const { email } = decodeToken({ token });
@@ -127,14 +141,14 @@ const userController = {
 						confirmation: {
 							type: "redirect",
 							return_url:
-								"https://marketing-helper.ru/payment?paymentStatus=success",
+								"https://startup-idea.ru/home?paymentStatus=success",
 						},
-						description: `Пополнение счёта на: ${amount}`,
+						description: `Пополнение счёта на: ${amount} RUB`,
 						receipt: {
 							customer: { fullName, phoneNumber, email },
 							items: [
 								{
-									description: `Транзакция на  "${amount}"`,
+									description: `Транзакция на "${amount}"`,
 									quantity: "1.00",
 									amount: {
 										currency: "RUB",
@@ -169,62 +183,67 @@ const userController = {
 		} catch (error) {
 			res.status(500).json({ error: "Ошибка сервера" });
 		}
-},
-confirmTransaction: async (req, res) => {
-  try {
-    const { uuid, paymentMethod, fullName, amount, phoneNumber } = req.body;
-    const [_, token] = req.headers.authorization.split(" ");
-    const { email } = decodeToken({ token });
+	},
+	confirmTransaction: async (req, res) => {
+		try {
+			const { uuid, paymentMethod, amount } = req.body;
+			const [_, token] = req.headers.authorization.split(" ");
+			const { email } = decodeToken({ token });
 
-    const userResult =
-      await sql`SELECT * FROM "users" WHERE email = ${email}`;
-    if (userResult.length === 0) {
-      return res.status(200).json({ message: "Невалидный токен" });
-    }
+			const userResult =
+				await sql`SELECT * FROM "users" WHERE email = ${email}`;
+			if (userResult.length === 0) {
+				return res.status(200).json({ message: "Невалидный токен" });
+			}
+			const user = userResult[0];
 
-    const updateSubscriptionAndPartnerIncome = async (status) => {
-      if (status === "paid" || status === "succeeded") {
+			const balance = Number(user.balance) + Number(amount);
 
-        await sql`UPDATE "user" SET "balance" = "balance" + ${amount} WHERE email = ${email}`;
-      }
-      return res.json({ status });
-    };
+			const updateSubscriptionAndPartnerIncome = async (status) => {
+				const date = new Date().toISOString();
 
-    if (paymentMethod === "card") {
-      const authString = `${process.env.YOOKASSA_SHOP_ID}:${process.env.YOOKASSA_API_KEY}`;
-      const url = `https://api.yookassa.ru/v3/payments/${uuid}`;
-      const headers = {
-        Authorization: `Basic ${Buffer.from(authString).toString(
-          "base64"
-        )}`,
-        "Idempotence-Key": uuidv4(), // Генерируем уникальный ключ идемпотентности
-        "Content-Type": "application/json",
-      };
-      return await fetch(url, {
-        method: "GET",
-        headers,
-      })
-        .then(async (response) => {
-          const data = await response.json();
-          return data;
-        })
-        .then(async (data) => {
-          if (!data.paid) return res.json({ paid: data.paid });
-          if (data.paid) {
-            return await updateSubscriptionAndPartnerIncome(
-              data.status
-            );
-          }
-        })
-        .catch((error) => {
-          return res.json({ error });
-        });
-    }
-    res.json({ message: "Произошла ошибка при оплате" });
-  } catch (error) {
-    res.status(500).json({ error: "Ошибка сервера" });
-  }
-}
+				if (status === "succeeded") {
+					await sql`UPDATE "users" SET "balance" = ${balance} WHERE email = ${email}`;
+					await sql`INSERT INTO "operations" (userid, type, date, status, amount) VALUES (${user.id}, 'Пополнение баланса', ${date}, 'Успешно', ${amount})`;
+				}
+				return res.json({ status, message: "Баланс успешно пополнен" });
+			};
+
+			if (paymentMethod === "card") {
+				const authString = `${process.env.YOOKASSA_SHOP_ID}:${process.env.YOOKASSA_API_KEY}`;
+				const url = `https://api.yookassa.ru/v3/payments/${uuid}`;
+				const headers = {
+					Authorization: `Basic ${Buffer.from(authString).toString(
+						"base64"
+					)}`,
+					"Idempotence-Key": uuidv4(), // Генерируем уникальный ключ идемпотентности
+					"Content-Type": "application/json",
+				};
+				return await fetch(url, {
+					method: "GET",
+					headers,
+				})
+					.then(async (response) => {
+						const data = await response.json();
+						return data;
+					})
+					.then(async (data) => {
+						if (!data.paid) return res.json({ paid: data.paid });
+						if (data.paid) {
+							return await updateSubscriptionAndPartnerIncome(
+								data.status
+							);
+						}
+					})
+					.catch((error) => {
+						return res.json({ error });
+					});
+			}
+			res.json({ message: "Произошла ошибка при оплате" });
+		} catch (error) {
+			res.status(500).json({ error: "Ошибка сервера" });
+		}
+	},
 };
 
 module.exports = userController;
